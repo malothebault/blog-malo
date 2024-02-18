13cc4bd7660cf2cbf0ad830cd6703413d557ff60
Malo Thebault <malothebault@lilo.org>
2024-02-18 16:59:03 +0100

    translate to french except for breadcrumps

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -30,10 +30,10 @@ export default function Datetime({
       </svg>
       {modDatetime ? (
         <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-          Updated:
+          Mis à jour le  
         </span>
       ) : (
-        <span className="sr-only">Published:</span>
+        <span className="sr-only">Publié le  </span>
       )}
       <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
         <FormattedDatetime
